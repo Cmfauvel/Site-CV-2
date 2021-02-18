dab3ac37294046a0c7bebfda61eac44d2cc71cee
Cmfauvel <Cm.fauvel@gmail.com>
2021-02-18 10:08:20 +0100

    creation p exp + css

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
-// let menu = document.querySelector('.menu--here');
 
-// menu.addEventListener('click', function(event) {
-//    console.log(menu)
-// });
-
-
+//Menu Burger
     window.onload = function() {
         let hiders = document.querySelectorAll('[data-js="hide"]');
       
@@ -19,3 +14,43 @@
         });
       });
     };
+
+    //Caroussel
+
+    const leftArrow = document.querySelector('.previous');
+const rightArrow = document.querySelector('.next');
+
+let images = document.getElementsByClassName('card');
+//déclare un tableau avec toutes mes images
+//console.log(images);
+let currentCard = 0;
+
+function nextSlide() {
+    // je cache l'image current
+    images[currentCard].className = 'card'
+    // je passe à la prochaine image
+    currentCard = (currentCard + 1) % images.length
+    // j'ajoute la classe current à ma nouvelle image pour la rendre visible
+    images[currentCard].className = 'card current'
+    console.log(currentCard)
+  }
+
+function prevSlide() {    
+    images[currentCard].className = 'card'    
+    currentCard = (currentCard - 1) % images.length
+    if (currentCard == -1) {
+      currentCard = images.length - 1
+    }    
+    images[currentCard].className = 'card current'
+    console.log(currentCard)
+  };
+
+rightArrow.addEventListener('click', () => {
+    images[currentCard].classList.remove('current');
+     nextSlide()
+ });
+ 
+ leftArrow.addEventListener('click', () => {
+    images[currentCard].classList.remove('current');
+    prevSlide()
+});
