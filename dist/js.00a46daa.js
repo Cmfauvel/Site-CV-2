ab484c6218fade19bc7e8472d2541ede4e3f7929
Cmfauvel <Cm.fauvel@gmail.com>
2021-03-14 00:26:59 +0100

    next arrow ok

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -129,70 +129,104 @@ window.onload = function () {
       hiderTarget.classList.toggle('--visible');
     });
   });
-}; //Caroussel
+}; //     //Caroussel
 
 
-var leftArrow1 = document.querySelector('#previous__formation');
-var rightArrow1 = document.querySelector('#next__formation');
-var leftArrow2 = document.querySelector('#previous__work');
-var rightArrow2 = document.querySelector('#next__work');
-var images = document.getElementsByClassName('card'); //déclare un tableau avec toutes mes images
+var leftArrow = document.querySelector('#previous__slide');
+var rightArrow = document.querySelector('#next__slide'); // const leftArrow2 = document.querySelector('#previous__work');
+// const rightArrow2 = document.querySelector('#next__work');
+
+var slideIndex = 1;
+var slides = document.getElementsByClassName('slide'); // let slidesWork = document.getElementsByClassName('slide2');
+//déclare un tableau avec toutes mes images
 //console.log(images);
 
-var currentCard = 0;
-var elementParcours = document.getElementsByClassName('text');
-var currentText = 0;
+showSlides();
 
-function nextSlide() {
-  // je cache l'image current
-  images[currentCard].className = 'card';
-  elementParcours[currentText].className = 'text'; // je passe à la prochaine image
-
-  currentCard = (currentCard + 1) % images.length;
-  currentText = (currentText + 1) % elementParcours.length; // j'ajoute la classe current à ma nouvelle image pour la rendre visible
-
-  images[currentCard].className = 'card current';
-  elementParcours[currentText].className = 'text current';
-  console.log(currentCard);
-}
-
-function prevSlide() {
-  images[currentCard].className = 'card';
-  elementParcours[currentText].className = 'text';
-  currentCard = (currentCard - 1) % images.length;
-  currentText = (currentText - 1) % elementParcours.length;
-
-  if (currentCard == -1 && currentText == -1) {
-    currentCard = images.length - 1;
-    currentText = elementParcours.length - 1;
+function showSlides(number) {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].classList.add('hide'); //slides[i].style.display = 'none'; équivalent
   }
 
-  images[currentCard].className = 'card current';
-  elementParcours[currentText].className = 'text current';
-  console.log(currentCard);
+  slides[slideIndex - 1].classList.add('show');
 }
 
-; // Ici ajouter une boucle pour que la fonction fasse des boucles sans sortir du tableau mais en reprenant au début ou à la fin selon la flêche sur laquelle on clique
+;
+rightArrow.addEventListener('click', function () {
+  var currentElement = document.querySelector('.show'); //ceci est mon premier élément
+
+  var nextSibling = currentElement.nextElementSibling; //pour cibler l'élément suivant
+
+  if (slideIndex < slides.length) {
+    currentElement.classList.remove('show');
+    nextSibling.classList.add('show');
+    slideIndex++;
+  } else {
+    slideIndex = 1;
+    slides[slideIndex - 1].classList.add('show');
+    slides[slides.length - 1].classList.remove('show');
+  }
+
+  console.log(slideIndex, slides.length);
+}); // leftArrow.addEventListener('click', () => {
+//   let currentElement = document.querySelector('.show');
+//       //ceci est mon premier élément
+//    let previousSibling = currentElement.previousElementSibling;
+//       //pour cibler l'élément suivant
+//    if(){
+//    } else {
+//    }
+//    console.log(slideIndex, slides.length);
+// });
+// function nextSlide() {
+//     // je cache l'image current
+//     images[currentCard].className = 'card__c1';
+//     elementParcours[currentText].className = 'text__c1';
+//     // je passe à la prochaine image
+//     currentCard = (currentCard + 1) % images.length;
+//     currentText = (currentText +1) % elementParcours.length;
+//     // j'ajoute la classe current à ma nouvelle image pour la rendre visible
+//     images[currentCard].className = 'card current';
+//     elementParcours[currentText].className = 'text current';
+//     console.log(currentCard)
+//   }
+// function prevSlide() {    
+//     images[currentCard].className = 'card'    ;
+//     elementParcours[currentText].className = 'text';
+//     currentCard = (currentCard - 1) % images.length;
+//     currentText = (currentText - 1) % elementParcours.length;
+//     if (currentCard == -1 && currentText == -1) {
+//       currentCard = images.length - 1;
+//       currentText = elementParcours.length - 1;
+//     }    
+//     images[currentCard].className = 'card current';
+//     elementParcours[currentText].className = 'text current';
+//     console.log(currentCard)
+//   };
+// Ici ajouter une boucle pour que la fonction fasse des boucles sans sortir du tableau mais en reprenant au début ou à la fin selon la flêche sur laquelle on clique
 // for (let i = 0; i < numberOfPassengers; i++) {
 //     console.log("Passager embarqué !");
 // }
-
-rightArrow1.addEventListener('click', function () {
-  images[currentCard].classList.remove('current');
-  nextSlide();
-});
-leftArrow1.addEventListener('click', function () {
-  images[currentCard].classList.remove('current');
-  prevSlide();
-});
-rightArrow2.addEventListener('click', function () {
-  images[currentCard].classList.remove('current');
-  nextSlide();
-});
-leftArrow2.addEventListener('click', function () {
-  images[currentCard].classList.remove('current');
-  prevSlide();
-});
+// rightArrow2.addEventListener('click', () => {
+//   images[currentCard].classList.remove('current');
+//    nextSlide()
+// });
+// leftArrow2.addEventListener('click', () => {
+//   images[currentCard].classList.remove('current');
+//   prevSlide()
+// });
+//Carroussel scolaire ==> c1
+// const nextArrowFormation = document.querySelector('#next__formation');
+// const previousArrowFormation = document.querySelector('#previous__formation');
+// let cardTableauFormation = document.getElementsByClassName('card__c1');
+// let textTableauFormation = document.getElementsByClassName('text__c1');
+// let currentCardFormation = 0;
+// let currentTextFormation = 0;
+// nextArrowFormation.addEventListener('click', () =>{
+//   for(let i = 0; i <= cardTableauFormation.length; i++){
+//     currentCardFormation
+//   }
+// })
 },{}],"C:/Users/cmfau/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -221,7 +255,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49790" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49530" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- js/index.js
+++ js/index.js
@@ -15,68 +15,118 @@
       });
     };
 
-    //Caroussel
+//     //Caroussel
 
-const leftArrow1 = document.querySelector('#previous__formation');
-const rightArrow1 = document.querySelector('#next__formation');
-const leftArrow2 = document.querySelector('#previous__work');
-const rightArrow2 = document.querySelector('#next__work');
+const leftArrow = document.querySelector('#previous__slide');
+const rightArrow = document.querySelector('#next__slide');
+// const leftArrow2 = document.querySelector('#previous__work');
+// const rightArrow2 = document.querySelector('#next__work');
+let slideIndex = 1;
+let slides = document.getElementsByClassName('slide');
+// let slidesWork = document.getElementsByClassName('slide2');
 
-let images = document.getElementsByClassName('card');
 //déclare un tableau avec toutes mes images
 //console.log(images);
-let currentCard = 0;
+showSlides();
 
-let elementParcours = document.getElementsByClassName('text');
-let currentText = 0;
-
-function nextSlide() {
-    // je cache l'image current
-    images[currentCard].className = 'card';
-    elementParcours[currentText].className = 'text';
-    // je passe à la prochaine image
-    currentCard = (currentCard + 1) % images.length;
-    currentText = (currentText +1) % elementParcours.length;
-    // j'ajoute la classe current à ma nouvelle image pour la rendre visible
-    images[currentCard].className = 'card current';
-    elementParcours[currentText].className = 'text current';
-    console.log(currentCard)
+function showSlides(number){
+  
+  for(let i = 0; i < slides.length; i++){
+    slides[i].classList.add('hide');
+    //slides[i].style.display = 'none'; équivalent
   }
+  slides[slideIndex - 1].classList.add('show');
+};
 
-function prevSlide() {    
-    images[currentCard].className = 'card'    ;
-    elementParcours[currentText].className = 'text';
-    currentCard = (currentCard - 1) % images.length;
-    currentText = (currentText - 1) % elementParcours.length;
-    if (currentCard == -1 && currentText == -1) {
-      currentCard = images.length - 1;
-      currentText = elementParcours.length - 1;
-    }    
-    images[currentCard].className = 'card current';
-    elementParcours[currentText].className = 'text current';
-    console.log(currentCard)
-  };
+rightArrow.addEventListener('click', () => {
+   let currentElement = document.querySelector('.show');
+      //ceci est mon premier élément
+   let nextSibling = currentElement.nextElementSibling;
+      //pour cibler l'élément suivant
+   if(slideIndex < slides.length){
+    currentElement.classList.remove('show');
+    nextSibling.classList.add('show');
+    slideIndex++;
+   } else {
+    slideIndex = 1;
+    slides[slideIndex - 1].classList.add('show');
+    slides[slides.length - 1].classList.remove('show');
+   }
+   console.log(slideIndex, slides.length);
+
+});
+
+// leftArrow.addEventListener('click', () => {
+//   let currentElement = document.querySelector('.show');
+//       //ceci est mon premier élément
+//    let previousSibling = currentElement.previousElementSibling;
+//       //pour cibler l'élément suivant
+//    if(){
+    
+//    } else {
+    
+//    }
+//    console.log(slideIndex, slides.length);
+// });
+
+// function nextSlide() {
+//     // je cache l'image current
+//     images[currentCard].className = 'card__c1';
+//     elementParcours[currentText].className = 'text__c1';
+//     // je passe à la prochaine image
+//     currentCard = (currentCard + 1) % images.length;
+//     currentText = (currentText +1) % elementParcours.length;
+//     // j'ajoute la classe current à ma nouvelle image pour la rendre visible
+//     images[currentCard].className = 'card current';
+//     elementParcours[currentText].className = 'text current';
+//     console.log(currentCard)
+//   }
+
+// function prevSlide() {    
+//     images[currentCard].className = 'card'    ;
+//     elementParcours[currentText].className = 'text';
+//     currentCard = (currentCard - 1) % images.length;
+//     currentText = (currentText - 1) % elementParcours.length;
+//     if (currentCard == -1 && currentText == -1) {
+//       currentCard = images.length - 1;
+//       currentText = elementParcours.length - 1;
+//     }    
+//     images[currentCard].className = 'card current';
+//     elementParcours[currentText].className = 'text current';
+//     console.log(currentCard)
+//   };
 
   // Ici ajouter une boucle pour que la fonction fasse des boucles sans sortir du tableau mais en reprenant au début ou à la fin selon la flêche sur laquelle on clique
   // for (let i = 0; i < numberOfPassengers; i++) {
   //     console.log("Passager embarqué !");
   // }
-rightArrow1.addEventListener('click', () => {
-    images[currentCard].classList.remove('current');
-     nextSlide()
- });
- 
- leftArrow1.addEventListener('click', () => {
-    images[currentCard].classList.remove('current');
-    prevSlide()
-});
 
-rightArrow2.addEventListener('click', () => {
-  images[currentCard].classList.remove('current');
-   nextSlide()
-});
 
-leftArrow2.addEventListener('click', () => {
-  images[currentCard].classList.remove('current');
-  prevSlide()
-});
+// rightArrow2.addEventListener('click', () => {
+//   images[currentCard].classList.remove('current');
+//    nextSlide()
+// });
+
+// leftArrow2.addEventListener('click', () => {
+//   images[currentCard].classList.remove('current');
+//   prevSlide()
+// });
+
+//Carroussel scolaire ==> c1
+
+// const nextArrowFormation = document.querySelector('#next__formation');
+// const previousArrowFormation = document.querySelector('#previous__formation');
+
+// let cardTableauFormation = document.getElementsByClassName('card__c1');
+// let textTableauFormation = document.getElementsByClassName('text__c1');
+
+// let currentCardFormation = 0;
+// let currentTextFormation = 0;
+
+// nextArrowFormation.addEventListener('click', () =>{
+//   for(let i = 0; i <= cardTableauFormation.length; i++){
+//     currentCardFormation
+//   }
+// })
+
+
